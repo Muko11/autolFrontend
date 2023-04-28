036158a0bba091532561f38063ca69164b3e7921
Ángel Cadenas Fijo <angel.cadenas@Aytos.local>
2023-04-28 14:21:44 +0200

    Añadiendo la API a App

--- src/App.svelte
+++ src/App.svelte
@@ -5,17 +5,25 @@
 	import Contenido from "./Contenido.svelte";
 	import Footer from "./Footer.svelte";
 
-	const URL = {
+	/* const URL = {
 		usuarios: "http://localhost:3000/autol/usuarios/",
 		login: "http://localhost:3000/autol/login/",
 		singup: "http://localhost:3000/autol/singup/",
 		account: "http://localhost:3000/autol/account/",
 		autoescuela: "http://localhost:3000/autol/autoescuela/",
 		profesor: "http://localhost:3000/autol/profesor/",
+	}; */
+
+	const URL = {
+		usuarios: "https://autolbackend.onrender.com/autol/usuarios/",
+		login: "https://autolbackend.onrender.com/autol/login/",
+		singup: "https://autolbackend.onrender.com/autol/singup/",
+		account: "https://autolbackend.onrender.com/autol/account/",
+		autoescuela: "https://autolbackend.onrender.com/autol/autoescuela/",
+		profesor: "https://autolbackend.onrender.com/autol/profesor/",
 	};
 
 	setContext("URL", URL);
-
 </script>
 
 <Router>
